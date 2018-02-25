a0c5391655115eaecbf7038795d3e3978dfec898
rafbajda <raf.baj1@wp.pl>
2018-02-25 21:44:48 +0100

    final

---
+++ src/Config/FireBase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyDAwnAofw-NO_pLEjfc3jGyD5-s-cVZm7Q",
+    authDomain: "react-flashcards-5bf08.firebaseapp.com",
+    databaseURL: "https://react-flashcards-5bf08.firebaseio.com",
+    projectId: "react-flashcards-5bf08",
+    storageBucket: "react-flashcards-5bf08.appspot.com",
+    messagingSenderId: "885759421904" 
+}
